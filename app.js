358ce006423fa80ebc5154bbd623ccbbb845a471
MickeySK03 <mixsorawit@gmail.com>
2023-12-21 21:44:55 +0700

    edit server

--- app.js
+++ app.js
@@ -5,17 +5,22 @@ const cors = require("cors");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
 
 app.use(cors());
 
-const io = require("socket.io")(server, {
+const io = new Server(server, {
   cors: {
     origin: "http://localhost:3505",
   },
 });
 
+io.on("connection", (socket) => {
+  console.log(`user ${socket.id} just connected`);
+});
+
 const PORT = process.env.PORT || 8000;
 
-http.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("server running on port", PORT);
 });
